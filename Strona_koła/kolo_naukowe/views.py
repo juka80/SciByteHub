a4d4b42f3458d4c2750eaaa8101080fe19b71237
Marta Pągielska <73945501+Ponczkowska@users.noreply.github.com>
2024-03-15 17:52:56 +0100

    Update views.py

--- Strona_koła/kolo_naukowe/views.py
+++ Strona_koła/kolo_naukowe/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def home(request):
     return render(request, 'kolo_naukowe/home.html')
+
+class ContactView(TemplateView):
+    template_name = "kontakt.html"
+
+class CoursesView(TemplateView):
+    template_name = "kursy.html"
+
+class AboutView(TemplateView):
+    template_name = "onas.html"
