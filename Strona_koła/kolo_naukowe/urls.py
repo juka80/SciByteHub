b6362c3f1a119cdc235fc148bf458d028c80ddb4
Marta Pągielska <73945501+Ponczkowska@users.noreply.github.com>
2024-03-15 17:50:36 +0100

    Update urls.py

--- Strona_koła/kolo_naukowe/urls.py
+++ Strona_koła/kolo_naukowe/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from . import views
+from .views import ContactView, CoursesView, AboutView
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', views.home, name='home'), 
+    path("kontakt/", ContactView.as_view(), name="kontact"),
+    path("kursy/", CoursesView.as_view(), name="kursy"),
+    path("onas/", AboutView.as_view(), name="onas"),
     #path('about/', views.about, name='about'),  
 ]
+
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
