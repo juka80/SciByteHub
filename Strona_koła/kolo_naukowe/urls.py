f9ea7877a0658125ee825fdcb52fcb38d951d54e
Julia Kuligowska <juliakul@gazeta.pl>
2024-03-18 20:39:36 +0100

    dodanie mozliwosci pobrania planu kursu

--- Strona_koła/kolo_naukowe/urls.py
+++ Strona_koła/kolo_naukowe/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import ContactView, CoursesView, AboutView, HomeView
+from .views import ContactView, CoursesView, AboutView, HomeView, download_pdf 
 from django.conf import settings
 from django.conf.urls.static import static 
+from . import views
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'), 
     path("kontakt/", ContactView.as_view(), name="kontakt"),
     path("kursy/", CoursesView.as_view(), name="kursy"),
     path("onas/", AboutView.as_view(), name="onas"),
-    #path('about/', views.about, name='about'),  
+    #path('about/', views.about, name='about'),
+	path('pobierz-pdf/', views.download_pdf, name='download_pdf'), 
 ]
 
 if settings.DEBUG:
--- Strona_koła/kolo_naukowe/views.py
+++ Strona_koła/kolo_naukowe/views.py
@@ -12,3 +12,14 @@ class CoursesView(TemplateView):
 
 class AboutView(TemplateView):
     template_name = "onas.html"
+
+from django.http import FileResponse
+import os
+from django.conf import settings
+
+def download_pdf(request):
+    filepath = os.path.join(settings.MEDIA_ROOT, 'Plan-kursu-Python-2024.pdf')  
+    pdf = open(filepath, 'rb')
+    response = FileResponse(pdf, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="%s"' % 'Plan-kursu-Python-2024.pdf'  
+    return response
