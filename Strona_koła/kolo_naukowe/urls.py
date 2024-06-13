170b72c19b9f9e4c74825295e21209c1bde3aca1
Julia Kuligowska <juliakul@gazeta.pl>
2024-06-13 10:28:22 +0200

    wszystko

--- Strona_koła/kolo_naukowe/urls.py
+++ Strona_koła/kolo_naukowe/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from .views import ContactView, CoursesView, AboutView, HomeView, download_pdf 
+from .views import ContactView, CoursesView, AboutView, HomeView, download_pdf, MembersView
 from django.conf import settings
 from django.conf.urls.static import static 
 from . import views
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'), 
-    path("kontakt/", ContactView.as_view(), name="kontakt"),
-    path("kursy/", CoursesView.as_view(), name="kursy"),
-    path("onas/", AboutView.as_view(), name="onas"),
-    #path('about/', views.about, name='about'),
-	path('pobierz-pdf/', views.download_pdf, name='download_pdf'), 
+    path('kontakt/', ContactView.as_view(), name='kontakt'),
+    path('kursy/', CoursesView.as_view(), name='kursy'),
+    path('onas/', AboutView.as_view(), name='onas'),
+    path('czlonkowie/', MembersView.as_view(), name='czlonkowie'),
+    path('pobierz-pdf/', views.download_pdf, name='download_pdf'), 
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Strona_koła/kolo_naukowe/views.py
+++ Strona_koła/kolo_naukowe/views.py
@@ -13,13 +13,16 @@ class CoursesView(TemplateView):
 class AboutView(TemplateView):
     template_name = "onas.html"
 
+class MembersView(TemplateView):
+    template_name = "czlonkowie.html"
+
 from django.http import FileResponse
 import os
 from django.conf import settings
 
 def download_pdf(request):
-    filepath = os.path.join(settings.MEDIA_ROOT, 'Plan-kursu-Python-2024.pdf')  
+    filepath = os.path.join(settings.MEDIA_ROOT, 'Plan-kursu-Python-2024.pdf')
     pdf = open(filepath, 'rb')
     response = FileResponse(pdf, content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="%s"' % 'Plan-kursu-Python-2024.pdf'  
+    response['Content-Disposition'] = 'attachment; filename="%s"' % 'Plan-kursu-Python-2024.pdf'
     return response
